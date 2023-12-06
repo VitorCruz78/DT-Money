2bbe8c26f0f18b94206dff86a9c67ccb7a97a3ee
VitorCruz78 <vitorluiz15012006@gmail.com>
2023-12-05 22:42:17 -0300

    Aplicando useMemo e conclusão do projeto

--- dt-money/src/pages/components/SearchForm/index.tsx
+++ dt-money/src/pages/components/SearchForm/index.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { useForm } from "react-hook-form";
 import { useContextSelector } from 'use-context-selector'
 import { MagnifyingGlass } from "phosphor-react";
@@ -12,7 +13,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
     const fetchTransactions = useContextSelector(TransactionsContext, (context) => {
         return context.fetchTransactions
     })
@@ -44,3 +45,5 @@ export function SearchForm() {
         </SearchFormContainer>
     )
 }
+
+export const SearchForm = memo(SearchFormComponent)
